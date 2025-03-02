b1da6bc1b1a5c4089338bbbfecf451963f47e660
parksiwoo2 <132458293+parksiwoo2@users.noreply.github.com>
2025-03-02 10:15:45 +0000

    03/02

--- direct/plan/views.py
+++ direct/plan/views.py
@@ -13,10 +13,8 @@ def calendar_view(request, year=None, month=None):
     month_days = cal.itermonthdays(year, month)
     events = Event.objects.filter(date__year=year, date__month=month)
 
-    event_dict = {event.date.day: event for event in events}
-    print(event_dict) #지금은 3월달인데 내가 추가한 이벤트가 없음 근데 추가하려니 갑자기 오류발광을 함
+    event_dict = {event.date.day: [event.title,event.description,event.id] for event in events}#date도 추가시켜야하는데 그러면 json으로 변환할때 직렬화가 안됨
     event=json.dumps(event_dict)
-    print(event)
     return render(request, "plan/calendar.html", {
         "year": year,
         "month": month,

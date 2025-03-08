cf6645dc45ed4cb1575aa404a746d4aa0eaf460c
parksiwoo2 <132458293+parksiwoo2@users.noreply.github.com>
2025-03-08 10:11:00 +0000

    07/10

--- direct/plan/models.py
+++ direct/plan/models.py
@@ -4,6 +4,6 @@ class Event(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True, null=True)
     date = models.DateField()
-
+    time=models.CharField(default='00:00:00',max_length=100)
     def __str__(self):
         return f"{self.title} ({self.date})"
--- direct/plan/views.py
+++ direct/plan/views.py
@@ -4,20 +4,22 @@ from .models import Event
 from .forms import EventForm
 import calendar
 import json
+
 def calendar_view(request, year=None, month=None):
     if year is None or month is None:
         year = now().year
         month = now().month
-    
+    day1=now().day
     cal = calendar.Calendar()
     month_days = cal.itermonthdays(year, month)
     events = Event.objects.filter(date__year=year, date__month=month)
 
-    event_dict = {event.date.day: [event.title,event.description,event.id] for event in events}#date도 추가시켜야하는데 그러면 json으로 변환할때 직렬화가 안됨
+    event_dict = {event.date.day: [event.title,event.description,event.id,event.time] for event in events}#date도 추가시켜야하는데 그러면 json으로 변환할때 직렬화가 안됨
     event=json.dumps(event_dict)
     return render(request, "plan/calendar.html", {
         "year": year,
         "month": month,
+        "day1": day1,
         "month_days": month_days,
         "event_dict": event,
         "calendar": calendar.month_name[month],
